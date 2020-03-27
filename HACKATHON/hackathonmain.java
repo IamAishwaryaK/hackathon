57363e1747a8d36829d6469bc976068aad73c987
IamAishwaryaK <46197625+IamAishwaryaK@users.noreply.github.com>
2020-03-27 14:08:19 +0530

    Update hackathonmain.java

--- HACKATHON/hackathonmain.java
+++ HACKATHON/hackathonmain.java
@@ -1,9 +1,8 @@
 import java.io.*;
 import org.json.simple.*;
 import java.sql.*;
-import java.text.DecimalFormat;
 
-public class Mock
+public class hackathonmain
 {
 	
 	public static void readfromfile()
@@ -11,14 +10,15 @@ public class Mock
 	  
 		try
 		{
-			long sum=0;
-			double avg,max=0;
-			long count=0;
-			long value=0;
+	        long sum=0;
+	        double avg,max=0;
+	        long count=0;
+                long value=0;
 		JSONObject json=new JSONObject();
-	    JSONObject memory = new JSONObject();
-	    JSONObject values = new JSONObject();
-        String fileName = "C:\\Users\\Aishwarya\\Documents\\memory.txt";
+	        JSONObject memory = new JSONObject();
+	        JSONObject values = new JSONObject();
+		//Read from file
+                String fileName = "C:\\Users\\Aishwarya\\Documents\\memory.txt";
 		File file = new File(fileName);
 		FileReader fr = new FileReader(file);
 		BufferedReader br = new BufferedReader(fr);
@@ -26,18 +26,15 @@ public class Mock
 		String line;
 		
 		
-		while((line = br.readLine()) != null){
+		while((line = br.readLine()) != null)
+		{
 			 memoryvalues=line.split("   ");
-			 
-			
-			 
-		    
-			 if(Long.parseLong(memoryvalues[1])!=value)
+			 if(Long.parseLong(memoryvalues[1])!=value)//Remove the repeated value
 			 {
-				   count++;
-				   sum= sum+Long.parseLong(memoryvalues[1]);
-				   System.out.println(memoryvalues[1]);
-			       values.put(count+"s",memoryvalues[1]);
+			       count++;//count increased when if condition is passed
+			       sum= sum+Long.parseLong(memoryvalues[1]);
+			       System.out.println(memoryvalues[1]);
+			       values.put(count+"s",memoryvalues[1]);//print values to json file
 			       value=Long.parseLong(memoryvalues[1]);
 			       
 			 }
@@ -51,43 +48,46 @@ public class Mock
 		}
 		 
 		avg=sum/count;
-		max=(max/1024);
-		max=(double)Math.round(max*100)/100;
+		max=(max/1024);//to MB
+		max=(double)Math.round(max*100)/100;//limit to two decimal places
 		System.out.println(max);
-		
 		avg=(avg/1024);
-		avg=(double)Math.round(avg*100)/100;
+		avg=(double)Math.round(avg*100)/100;//limit to two decimal places
 		System.out.println(avg);
 		br.close();
-	     Double average=new Double(avg);
-	     Double maximum=new Double(max);
+	        Double average=new Double(avg);
+	        Double maximum=new Double(max);
 		Object Sample="Sample";
 		json.put("Usecasename", Sample);
 		json.put("Values:", values);
-        json.put("Averagememory(MB)", average);
-        json.put("Maximummemory(MB)", maximum);
-		FileWriter file1 = new FileWriter("C:\\Users\\Aishwarya\\Documents\\hackathonmemory.json");
-			 
-        file1.write(json.toJSONString());
-           
-        file1.close();
+                json.put("Averagememory(MB)", average);
+                json.put("Maximummemory(MB)", maximum);
+		FileWriter file1 = new FileWriter("C:\\Users\\Aishwarya\\Documents\\hackathonmemory.json");//writing it to file
+		file1.write(json.toJSONString());
+                file1.close();
 		
-           
+           //opening DB connection
             String JdbcURL = "jdbc:mariadb://localhost:3306/demo?useSSL=false";
             String Username = "root";
             String password = "1234Aish*";
 		    Connection connection = DriverManager.getConnection(JdbcURL,Username,password);
 		    System.out.println("CONNECTION ESTABLISHED...");
-		    Statement query=connection.createStatement();
-		    String sql = "CREATE TABLE Memorydetails" +
+			
+			//Table creation query
+			
+		 Statement query=connection.createStatement();
+		 String sql = "CREATE TABLE Memorydetails" +
                  "(Transactionname VARCHAR(255), " +
                  " Maximum VARCHAR(255), " + 
                  " Average VARCHAR(255))";
 		 query.executeUpdate(sql);
-		System.out.println("TABLE CREATED...");
-				 query.executeUpdate("INSERT INTO Memorydetails (Transactionname, Maximum, Average) "
+		 System.out.println("TABLE CREATED...");
+			
+			//Insertion query
+			
+	          query.executeUpdate("INSERT INTO Memorydetails (Transactionname, Maximum, Average) "
 				          +"VALUES ('Sample','" +max+ "','" +avg+ "')");
-				          System.out.println("VALUES UPDATED...");
+		  System.out.println("VALUES UPDATED...");
            }catch(Exception e)
            {
         	   System.out.println(e);
@@ -97,8 +97,7 @@ public class Mock
 	public static void main(String[] args)
 	{
 		readfromfile();
-		//todatabase();
-		//System.out.println("a");
+		
 	}
 	
 }
